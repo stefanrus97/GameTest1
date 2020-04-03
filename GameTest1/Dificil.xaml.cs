474da2e57fa67ae31d7319b5b9ca8c26bb9f7fa8
Stefan Rus <stefan.97.rus@gmail.com>
2020-04-03 13:27:47 +0300

    Add project files.

---
+++ GameTest1/Dificil.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameTest1
+{
+    /// <summary>
+    /// Interaction logic for Dificil.xaml
+    /// </summary>
+    public partial class Dificil : Window
+    {
+        public Dificil()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
